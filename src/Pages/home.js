d5eff31ee7d02f7d901602d7746a369362b31b92
Hamza Asif <hamzaasif0987@gmail.com>
2020-05-29 21:13:15 +0500

    footer

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,12 +1,7 @@
 import React , { Component} from 'react'
 import {
-  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBFormInline,
-MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem,
-MDBView,
-MDBContainer,
-MDBCarousel,
-MDBCarouselInner,
-MDBCarouselItem
+  MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavbarToggler, MDBCollapse, MDBFormInline,
+MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem,MDBIcon
 
   } from "mdbreact";
 
@@ -23,8 +18,8 @@ class home extends Component {
     isOpen:false
   }
 
-  toggleCollaspe = ()=>{
-    this.setState({isOpen:!this.state.isOpen})
+  toggleCollapse = ()=>{
+    this.setState({isOpen: !this.state.isOpen})
   }
 
   render(){
@@ -37,17 +32,21 @@ class home extends Component {
       <Router>
 
       <MDBNavbar color="rgba-black-light "   dark expand="md"  scrolling fixed="top" >
-        <MDBNavbarBrand href="/">
-        <img
+
+        <MDBNavbarBrand href="/" className="font-weight-bold">
+        <MDBIcon icon="globe-africa"  />  Space Blink
+        
+        {/* <img
               className="d-block w-50"
               src={require('../img/logo.png')}
               alt=""
-              />
-
+              /> */}
+              
         </MDBNavbarBrand>
 
-        <MDBNavbarToggler  onClick={this.toggleCollapse} />
+        <MDBNavbarToggler onClick={this.toggleCollapse} />
         <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+
 
           <MDBNavbarNav left>
 
@@ -58,7 +57,7 @@ class home extends Component {
                   </strong>
                 </MDBDropdownToggle>
                 <MDBDropdownMenu>
-                  <MDBDropdownItem href="#!">Beach Party Places</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Beach Party Places</MDBDropdownItem>
                   <MDBDropdownItem href="#!">Beach Love Stories</MDBDropdownItem>
                   <MDBDropdownItem href="#!">Map</MDBDropdownItem>
                 </MDBDropdownMenu>
@@ -71,12 +70,12 @@ class home extends Component {
                   </span>
                 </MDBDropdownToggle>
                 <MDBDropdownMenu>
-                  <MDBDropdownItem href="#!">Visa Guides</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Travel Guides</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Food</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Hotem Reviews</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Start a Travel Blog</MDBDropdownItem>
-                  <MDBDropdownItem href="#!">Sustainable Travel</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Visa Guides</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Travel Guides</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Food</MDBDropdownItem>
+                  <MDBDropdownItem   href="#!">Hotel Reviews</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Start a Travel Blog</MDBDropdownItem>
+                  <MDBDropdownItem  href="#!">Sustainable Travel</MDBDropdownItem>
                 </MDBDropdownMenu>
               </MDBDropdown>
 
@@ -148,12 +147,172 @@ class home extends Component {
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src={require('../img/cover 3.jpg')}/>
+      <img class="d-block w-100" alt= "Responsive" src={require('../img/cover 3.jpg')}/>
     </div>
   </div>
 </div>
 
+        
+
+        {/* Footer */}
+<br/><br/>
+
+
+
+<footer class="page-footer font-small brown lighten-1  pt-4">
+
+
+<div class="container">
+
+<div class="row">
+
+
+  <div class="col-md-12 py-5">
+    <div class="mb-5 flex-center">
+
+      
+      <a class="fb-ic">
+        <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+    
+      <a class="tw-ic">
+        <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="gplus-ic">
+        <i class="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+     
+      <a class="ins-ic">
+        <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+      </a>
+      
+      <a class="pin-ic">
+        <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+      </a>
     </div>
+  </div>
+  
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+  <div class="container">
+
+    <div class="row">
+
+    
+   
+      <div class="col-lg-2 col-md-12 mb-4">
+        
+
+
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(73).jpg" class="img-fluid"
+            alt=""/>
+          {/* <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a> */}
+        </div>
+
+      </div>
+ 
+
+
+      <div class="col-lg-2 col-md-6 mb-4">
+
+  
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(78).jpg" class="img-fluid"
+            alt=""/>
+          {/* <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a> */}
+        </div>
+
+      </div>
+    
+      <div class="col-lg-2 col-md-6 mb-4">
+
+        
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(79).jpg" class="img-fluid"
+            alt=""/>
+          {/* <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a> */}
+        </div>
+
+      </div>
+ 
+
+    
+      <div class="col-lg-2 col-md-12 mb-4">
+
+     
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(81).jpg" class="img-fluid"
+            alt=""/>
+
+          {/* <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a> */}
+        </div>
+
+      </div>
+     
+     
+      <div class="col-lg-2 col-md-6 mb-4">
+
+     
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(82).jpg" class="img-fluid"
+            alt=""/>
+
+          {/* <a href="">
+            <div class="mask rgba-white-light"></div>
+          </a> */}
+
+        </div>
+
+      </div>
+     
+
+      
+      <div class="col-lg-2 col-md-6 mb-4">
+
+        <div class="view overlay z-depth-1-half">
+          <img src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(84).jpg" class="img-fluid"
+            alt=""/>
+
+         
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+  
+
+ 
+  <div className="footer-copyright text-center py-3">© 2020 Copyright: All rights reserved
+  </div>
+ 
+
+</footer>
+</div>
+
+
     )
   }
 }
